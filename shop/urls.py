fe36a15a1504491ec951e8e1c541f9f6776f648c
Hem Shrestha <hereshem@gmail.com>
2018-08-25 23:21:46 +0545

    list view added, full website browsable

--- shop/urls.py
+++ shop/urls.py
@@ -11,6 +11,7 @@ urlpatterns = [
     path('login/', views.mylogin, name="login"),
     path('logout/', views.mylogout, name="logout"),
     path('search/', views.product_search, name="search"),
+    path('search_api/', views.product_search_api, name="search_api"),
     path('add_to_cart/', views.add_to_cart, name="add_to_cart"),
     path('my_cart/', views.my_cart, name="my_cart"),
     path('checkout/', views.checkout, name="checkout"),
--- shop/views.py
+++ shop/views.py
@@ -1,5 +1,6 @@
 from django.contrib.auth import authenticate, login, logout
 from django.db.models import Q
+from django.http import JsonResponse
 from django.shortcuts import render, redirect, get_object_or_404
 
 # Create your views here.
@@ -11,29 +12,25 @@ from shop.models import Category, Product, Review
 from shop.serializers import ProductSerializer
 
 
+def get_payload(request, title=""):
+    return {
+        "title": title,
+        "categories": Category.objects.filter(active=True),
+        "carts": Product.objects.filter(slug__in=request.session.get('items', []))
+    }
+
+
 def home(request):
-    categories = Category.objects.all()
-    products = Product.objects.all()
-    carts = Product.objects.filter(slug__in=request.session.get('items', []))
-    data = {
-        "categories": categories,
-        "products": products,
-        "carts": carts,
-            }
+    data = get_payload(request, "Home")
+    data["products"] = Product.objects.filter(active=True)
     return render(request, "shop/index.html", data)
 
 
 def details(request, slug):
     product = get_object_or_404(Product, slug=slug)
-    categories = Category.objects.all()
-    reviewform = ReviewForm()
-    carts = Product.objects.filter(slug__in=request.session.get('items', []))
-    data = {
-        "product": product,
-        "reviewform": reviewform,
-        "carts": carts,
-        "categories": categories
-    }
+    data = get_payload(request, product.title)
+    data["product"] = product
+    data["reviewform"] = ReviewForm()
     return render(request, "shop/details.html", data)
 
 
@@ -57,12 +54,11 @@ def signup(request):
         form = SignUpForm(request.POST)
         if form.is_valid():
             user = form.save(commit=False)
-            user.is_active = True
             user.save()
             return redirect('shop:login')
     else:
         form = SignUpForm()
-        return render(request, "shop/signup.html", {"form": form})
+    return render(request, "shop/signup.html", {"form": form})
 
 
 def mylogin(request):
@@ -85,35 +81,24 @@ def mylogout(request):
 
 
 @api_view(['GET'])
+def product_search_api(request):
+    query = request.GET.get("q", "")
+    products = Product.objects.filter(Q(title__contains=query) | Q(description__contains=query))
+    serializer = ProductSerializer(products, many=True)
+    return Response(serializer.data)
+
+
 def product_search(request):
-    if request.method == 'GET':
-        query = request.GET.get("q", "")
-        products = Product.objects.filter(Q(title__contains=query) | Q(description__contains=query))
-        serializer = ProductSerializer(products, many=True)
-        return Response(serializer.data)
+    query = request.GET.get("q", "")
+    data = get_payload(request, query + " - search result")
+    data["products"] = Product.objects.filter(Q(title__contains=query) | Q(description__contains=query))
+    return render(request, 'shop/products.html', data)
 
 
-@api_view(['POST'])
-def add_to_cart(request):
-    if request.method == 'POST':
-        slug = request.POST.get("slug", "")
-        product = get_object_or_404(Product, slug=slug)
-        items = request.session.get('items', [])
-        if slug not in items:
-            amount = request.session.get('cart_amount', 0.0)
-            items.append(product.slug)
-            request.session['items'] = items
-            request.session['cart_amount'] = amount + float(product.price)
-        serializer = ProductSerializer(product)
-        return Response(serializer.data)
-
-
-@api_view(['GET'])
 def my_cart(request):
-    if request.method == 'GET':
-        products = Product.objects.filter(slug__in=request.session.get('items', []))
-        serializer = ProductSerializer(products, many=True)
-        return Response(serializer.data)
+    data = get_payload(request, "My cart")
+    data["products"] = Product.objects.filter(slug__in=request.session.get('items', []))
+    return render(request, 'shop/products.html', data)
 
 
 def checkout(request):
@@ -122,9 +107,27 @@ def checkout(request):
     return redirect("shop:home")
 
 
-@api_view(['GET'])
 def categories(request, slug):
     category = get_object_or_404(Category, slug=slug)
-    products = Product.objects.filter(category=category)
-    serializer = ProductSerializer(products, many=True)
-    return Response(serializer.data)
+    data = get_payload(request, category.title)
+    data["products"] = Product.objects.filter(category=category)
+    return render(request, "shop/products.html", data)
+
+
+def add_to_cart(request):
+    if request.method == 'POST':
+        slug = request.POST.get("slug", "")
+        product = Product.objects.filter(slug=slug).first()
+        if product:
+            items = request.session.get('items', [])
+            if slug not in items:
+                items.append(product.slug)
+                amount = request.session.get('cart_amount', 0.0) + float(product.price)
+                request.session['items'] = items
+                request.session['cart_amount'] = amount
+                return JsonResponse({"status": "ok", "message": product.title + " added to cart",
+                                     "count": len(items), "amount": amount})
+            return JsonResponse({"status": "fail", "message": "Item already added"})
+        return JsonResponse({"status": "fail", "message": "Item not found"})
+    return JsonResponse({"status": "fail", "message": "Only post method allowed"})
+
